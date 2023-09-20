322ce67974e34530741bdf4bd99f2d03e8d61af4
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-09-19 18:41:48 -0700

    Done

--- main-file.c
+++ main-file.c
@@ -4,23 +4,26 @@
 
 #define MAX_COMMAND_LENGTH 256
 
-int main() {
-    char command[MAX_COMMAND_LENGTH];
+int main(void)
+{
+	char command[MAX_COMMAND_LENGTH];
 
-    while (1) {
-        printf("#ourFirstShell$ ");
-        fflush(stdout);
+	while (1)
+	{
+		printf("#ourFirstShell$ ");
+		fflush(stdout);
 
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            printf("\nThank you for your time, Bye!\n");
-            break;
-        }
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			printf("\nThank you for your time, Bye!\n");
+			break;
+		}
 
-        command[strcspn(command, "\n")] = '\0';
+		command[strcspn(command, "\n")] = '\0';
 
-        execute_command(command);
-    }
+		execute_command(command);
+	}
 
-    return 0;
+	return (0);
 }
 
--- the_shell.c
+++ the_shell.c
@@ -5,22 +5,29 @@
 #include <sys/wait.h>
 #include "the_shell-header.h"
 
-void execute_command(char *command) {
-    pid_t pid;
-    int status;
+void execute_command(char *command)
+{
+	pid_t pid;
+	int status;
 
-    pid = fork();
+	pid = fork();
 
-    if (pid == 0) {
-        char *args[] = {command, NULL};
-        if (execvp(command, args) == -1) {
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-    } else if (pid < 0) {
-        perror("Fork failed");
-    } else {
-        waitpid(pid, &status, 0);
-    }
+	if (pid == 0)
+	{
+		char *args[] = {command, NULL};
+		if (execvp(command, args) == -1)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("Fork failed");
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
 
