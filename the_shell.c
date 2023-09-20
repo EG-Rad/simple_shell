3a7f5587901e249bcbf947a8fe15891b4aa2e514
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-09-20 00:56:32 -0700

    error in shell.c file

--- the_shell.c
+++ the_shell.c
@@ -1,4 +1,5 @@
 #include "the_shell-header.h"
+
 /*
  * File: the_shell.c
  * Description: A simple shell program that reads and executes user-entered commands.
@@ -7,27 +8,30 @@
 
 void execute_command(char *command)
 {
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	pid = fork();
+    pid = fork();
 
-	if (pid == 0)
-	{
-		char *args[] = {command, NULL};
-		if (execvp(command, args) == -1)
-		{
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else if (pid < 0)
-	{
-		perror("Fork failed");
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
+    if (pid == 0)
+    {
+        char *args[2];
+        args[0] = command;
+        args[1] = NULL;
+
+        if (execvp(command, args) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("Fork failed");
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
 }
 
