87b445b9b941973bbd51ba5369f07367d8d85f0e
Tshegofatso <106757261+Tshegofatso-Tswai@users.noreply.github.com>
2023-09-21 05:50:06 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,34 +1,80 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define MAX_COMMAND_LENGTH 256
-#define MAX_ARGUMENTS 64
-#define BUFFER_SIZE 1024
-#define MAX_ALIASES 64
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
 
-// Structure to represent an alias
-typedef struct {
-    char *name;
-    char *value;
-} Alias;
 
-extern Alias aliases[MAX_ALIASES]; // Array to store aliases
-extern int num_aliases; // Number of aliases defined
-extern int last_status; // Store the exit status of the last command
+/*constants*/
+#define EXTERNAL_COMMAND 1
+#define INTERNAL_COMMAND 2
+#define PATH_COMMAND 3
+#define INVALID_COMMAND -1
 
-// Function prototypes
-void execute_command(char *command, char *arguments[]);
-ssize_t read_line(char **lineptr);
-void tokenize_input(char *input, char *tokens[]);
-int custom_setenv(char *variable, char *value);
-int custom_unsetenv(char *variable);
-int custom_cd(char *directory);
-void execute_separated_commands(char *input);
-void execute_logical_commands(char *input);
-void print_aliases(char *name);
-int define_alias(char *name, char *value);
-int custom_alias(char *arguments[]);
-char *replace_variables(char *command);
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
-#endif
+/**
+ *struct map - a struct that maps a command name to a function 
+ *
+ *@command_name: name of the command
+ *@func: the function that executes the command
+ */
+
+typedef struct map
+{
+	char *command_name;
+	void (*func)(char **command);
+} function_map;
+
+extern char **environ;
+extern char *line;
+extern char **commands;
+extern char *shell_name;
+extern int status;
+
+/*helpers*/
+void print(char *, int);
+char **tokenizer(char *, char *);
+void remove_newline(char *);
+int _strlen(char *);
+void _strcpy(char *, char *);
+
+/*helpers2*/
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+int _strspn(char *, char *);
+int _strcspn(char *, char *);
+char *_strchr(char *, char);
+
+/*helpers3*/
+char *_strtok_r(char *, char *, char **);
+int _atoi(char *);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void ctrl_c_handler(int);
+void remove_comment(char *);
+
+/*utils*/
+int parse_command(char *);
+void execute_command(char **, int);
+char *check_path(char *);
+void (*get_func(char *))(char **);
+char *_getenv(char *);
+
+/*built_in*/
+void env(char **);
+void quit(char **);
+
+/*main*/
+extern void non_interactive(void);
+extern void initializer(char **current_command, int type_command);
+
+#endif /*SHELL_H*/
 
